041873a72c00d3bd1c1cd49903835d525f9a4f52
aladdinalizada <tu7c5zneb@code.edu.az>
2023-03-23 01:53:30 +0400

    first commit in svelte

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import LogoText from "./LogoText.svelte";
+  let image = {
+    src: "https://media.giphy.com/media/3o7TKsQ8UQhtVrZ3Di/giphy.gif",
+    alt: "kopek gif resmi",
+    title: "kopek gif",
+  };
+
+  let name = "AladdinAlizaa";
+</script>
+
+<LogoText {name} />
+<img {...image} alt={image.alt} />
---
+++ src/LogoText.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+</script>
+
+<div>
+  <h1>
+    Hello {name}!
+  </h1>
+</div>
+
+<style lang="scss">
+  @import "styles/variables.scss";
+
+  div {
+    h1 {
+      color: red;
+      //   font-weight: @font-weight;
+      font-family: $font-style;
+    }
+  }
+</style>
